7208d9eb0878a3b2b8e729c426f34e82f0fdb156
André Chaves <andrelmchaves@gmail.com>
2025-04-10 13:12:21 -0300

    publish

--- assets/index-BnNUM6VK.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./index-95ia97KA.js";const i=function(){return o.jsx("div",{children:"index"})};export{i as component};
---
+++ assets/index-GIZ6-21q.js
@@ -0,0 +1,1 @@
+import{q as o}from"./index-B3kVX-j3.js";const i=function(){return o.jsx("div",{children:"index"})};export{i as component};
